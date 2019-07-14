26d998780474d714b337351db798163c50badf17
Mev <36679013+Hirss@users.noreply.github.com>
2019-07-14 11:08:47 +0200

    Update TeleportTask.php

--- src/Mev/task/TeleportTask.php
+++ src/Mev/task/TeleportTask.php
@@ -4,16 +4,28 @@ namespace Mev\task;
 
 use Mev\Main;
 use Mev\task\ScoreboardTask;
+
+use pocketmine\block\Block;
+use pocketmine\block\BlockFactory;
+
 use pocketmine\item\Item;
 use pocketmine\item\ItemFactory;
 use pocketmine\item\Armor;
+
+use pocketmine\item\enchantment\Enchantment;
+use pocketmine\item\enchantment\EnchantmentInstance;
+
 use pocketmine\inventory\PlayerInventory;
 use pocketmine\inventory\ArmorInventory;
+
 use pocketmine\entity\Effect;
 use pocketmine\entity\EffectInstance;
+
 use pocketmine\scheduler\Task;
+
 use pocketmine\Player;
 use pocketmine\Server;
+
 use pocketmine\level\Position;
 use pocketmine\utils\TextFormat as TF;
 use pocketmine\math\Vector3;
@@ -35,21 +47,24 @@ class TeleportTask extends Task {
   			$this->player->sendMessage(TF::YELLOW . "[FFA] You will teleported in " . $this->start . " seconds");
 		} 
         if($this->start == 1) {
-        	$x = mt_rand(20, 40);
+        	    $x = mt_rand(-12, 60);
            	 $y = mt_rand(40, 41);
-           	 $z = mt_rand(20, 40);
+           	 $z = mt_rand(-14, 50);
            
           	  $arenalevel = $this->plugin->getServer()->getLevelByName("ffa");
        	 	$arenaspawn = $arenalevel->getSafeSpawn();
-       	 	$this->player->teleport($arenaspawn, $y, $z);
+       	 	$this->player->teleport(new Position($x, $y, $z, $arenalevel));
             	$this->player->sendMessage(TF::DARK_PURPLE . "You have joined FFA-1.");
                 $this->player->setGamemode(0);
+                $this->player->setHealth(20);
+                $this->player->setFood(20);
                 $this->player->getInventory()->clearAll();
-      		  $helmet = Item::get(306); 
-				$chestplate = Item::get(307);
-				$leggings = Item::get(308); 
-				$boots = Item::get(309); 
+      		  $helmet = Item::get(310);
+				$chestplate = Item::get(311);
+				$leggings = Item::get(312); 
+				$boots = Item::get(313); 
 				$sword = ItemFactory::get(Item::DIAMOND_SWORD); 
+				$apple = ItemFactory::get(Item::GOLDEN_APPLE);
 				$sword->clearCustomName(); 
 				$sword->setCustomName("§bSword"); 
 				$this->player->getArmorInventory()->setHelmet($helmet); 
@@ -57,6 +72,7 @@ class TeleportTask extends Task {
 				$this->player->getArmorInventory()->setLeggings($leggings); 
 				$this->player->getArmorInventory()->setBoots($boots); 
 				$this->player->getInventory()->addItem($sword);
+	   		 $this->player->getInventory()->addItem($apple);
         }
     }
 }
