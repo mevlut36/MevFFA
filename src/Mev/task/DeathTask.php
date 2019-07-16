c817a09617bc619f66c949a224e5a2b98a7d1b4b
Mev <36679013+Hirss@users.noreply.github.com>
2019-07-16 18:50:45 +0200

    Add lobby item when you die

--- src/Mev/task/DeathTask.php
+++ src/Mev/task/DeathTask.php
@@ -35,12 +35,20 @@ class DeathTask extends Task {
   			$this->player->sendMessage(TF::YELLOW . "[FFA] You will be revived in " . $this->back . " seconds");
 		} 
         if($this->back == 1) {
-          	  $arenalevel = $this->plugin->getServer()->getLevelByName("world");
+		$inv = $this->player->getInventory();
+        	$ffa = Item::get(399, 0, 1);
+        	$ffa->setCustomName("§l§4FFA");
+        	$pvp = Item::get(265, 0, 1);
+        	$pvp->setCustomName("§l§21v1");
+        	$arenalevel = $this->plugin->getServer()->getLevelByName("world");
        	 	$arenaspawn = $arenalevel->getSafeSpawn();
        	 	$this->player->teleport($arenaspawn);
-            	$this->player->sendMessage(TF::DARK_PURPLE . "You have been revived.");
+                $this->player->sendMessage(TF::DARK_PURPLE . "You have been revived.");
                 $this->player->setGamemode(2);
-                $this->player->getInventory()->clearAll();
+		$this->player->getInventory()->clearAll();
+		$this->player->getArmorInventory()->clearAll();
+		$inv->setItem(4, $ffa);
+		$inv->setItem(0, $pvp);
         }
     }
 }
